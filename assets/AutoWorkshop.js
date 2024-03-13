d561ada9bbd75d959eaa4b1df9a309baf91c8526
qthais <thaiallb63@gmail.com>
2024-03-13 10:55:39 +0700

    add slider

--- assets/AutoWorkshop.js
+++ assets/AutoWorkshop.js
@@ -61,3 +61,45 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+//Doorstep service part
+var doorstepList= document.querySelector('.doorstep-slider .doorstep-list')
+var doorstepItem= document.querySelectorAll('.doorstep-list .doorstep-item')
+var dots=document.querySelectorAll('.dots li')
+var prev=document.getElementById('prev')
+var next=document.getElementById('next')
+let active=0;
+let lengthItems=doorstepItem.length-1
+next.onclick= function(){
+    if(active+1>lengthItems){
+        active=0;
+    }
+    else{
+        active+=1;
+    }
+    reloadSlider();
+}
+prev.onclick=function(){
+    if(active==0){
+        active=lengthItems
+    }else{
+        active-=1
+    }
+    reloadSlider();
+}
+dots.forEach(function(li,index){
+    li.addEventListener('click',function(){
+        active=index;
+        reloadSlider();
+    })
+})
+
+let refreshSlider= setInterval(()=>next.click(),4000)
+function reloadSlider(){
+    let checkLeft=doorstepItem[active].offsetLeft
+    doorstepList.style.left=-checkLeft+'px'
+    let previousDot= document.querySelector('.doorstep-slider .dots li.active')
+    previousDot.classList.remove('active')
+    dots[active].classList.add('active')
+    clearInterval(refreshSlider);
+    refreshSlider= setInterval(()=>next.click(),4000)
+}
