9c4c1bed483b74648382831437c92408bbfcc24e
qthais <thaiallb63@gmail.com>
2024-04-27 23:44:05 +0700

    clean code in js file

--- assets/AutoWorkshop.js
+++ assets/AutoWorkshop.js
@@ -3,34 +3,35 @@ const buyBtns = document.querySelectorAll('.booking-button')
 const modal = document.querySelector('.js-modal')
 const modalContainer = document.querySelector('.js-modal-container')
 const modalClose = document.querySelector('.js-modal-close')
-
-function showBuyTickets() {
-    modal.classList.add('open')
+function addOpenFlex(element) {
+    element.classList.add('openFlex')
 }
-function hidenBuyTickets() {
-    modal.classList.remove('open')
+function removeOpenFlex(element) {
+    element.classList.remove('openFlex')
+}
+function addOpenBlock(element) {
+    element.classList.add('openBlock')
+}
+function removeOpenBlock(element) {
+    element.classList.remove('openBlock')
+}
+function stopProba(container,modal) {
+    modal.addEventListener('click', () => removeOpenFlex(modal))
+    container.addEventListener('click', function (event) {
+        event.stopPropagation()
+    })
 }
 for (const buyBtn of buyBtns) {
-    buyBtn.addEventListener('click', showBuyTickets)
+    buyBtn.addEventListener('click', () => addOpenFlex(modal))
 }
-modalClose.addEventListener('click', hidenBuyTickets)
-modal.addEventListener('click', hidenBuyTickets)
-modalContainer.addEventListener('click', function (event) {
-    event.stopPropagation()
-})
+modalClose.addEventListener('click', () => removeOpenFlex(modal))
+stopProba(modalContainer,modal)
 //Login part
 const loginHeader = document.querySelector('.login-section')
-const loginModal=document.querySelector('.login-modal')
-const loginContainer=document.querySelector('.login-modal .container')
-loginHeader.addEventListener('click', function () {
-    loginModal.classList.add('open')
-})
-loginModal.addEventListener('click',function(){
-    loginModal.classList.remove('open')
-})
-loginContainer.addEventListener('click', function (event) {
-    event.stopPropagation()
-})
+const loginModal = document.querySelector('.login-modal')
+const loginContainer = document.querySelector('.login-modal .container')
+loginHeader.addEventListener('click', () => addOpenFlex(loginModal))
+stopProba(loginContainer,loginModal)
 
 //Slider part
 var sliderHref = document.querySelector('#slider')
@@ -61,45 +62,54 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+//CartListpart
+const htmlElement = document.body;
+var cartButton = document.querySelector('#header .cart-section')
+var cartList = document.querySelector('#header .cart_login .cart-list')
+cartButton.addEventListener('click', () => addOpenBlock(cartList))
+htmlElement.addEventListener('click', () => removeOpenBlock(cartList))
+    cartButton.addEventListener('click', function (event) {
+        event.stopPropagation()
+    })
 //Doorstep service part
-var doorstepList= document.querySelector('.doorstep-slider .doorstep-list')
-var doorstepItem= document.querySelectorAll('.doorstep-list .doorstep-item')
-var dots=document.querySelectorAll('.dots li')
-var prev=document.getElementById('prev')
-var next=document.getElementById('next')
-let active=0;
-let lengthItems=doorstepItem.length-1
-next.onclick= function(){
-    if(active+1>lengthItems){
-        active=0;
+var doorstepList = document.querySelector('.doorstep-slider .doorstep-list')
+var doorstepItem = document.querySelectorAll('.doorstep-list .doorstep-item')
+var dots = document.querySelectorAll('.dots li')
+var prev = document.getElementById('prev')
+var next = document.getElementById('next')
+let active = 0;
+let lengthItems = doorstepItem.length - 1
+next.onclick = function () {
+    if (active + 1 > lengthItems) {
+        active = 0;
     }
-    else{
-        active+=1;
-    }
-    reloadSlider();
-}
-prev.onclick=function(){
-    if(active==0){
-        active=lengthItems
-    }else{
-        active-=1
+    else {
+        active += 1;
     }
     reloadSlider();
 }
-dots.forEach(function(li,index){
-    li.addEventListener('click',function(){
-        active=index;
+prev.onclick = function () {
+    if (active == 0) {
+        active = lengthItems
+    } else {
+        active -= 1
+    }
+    reloadSlider();
+}
+dots.forEach(function (li, index) {
+    li.addEventListener('click', function () {
+        active = index;
         reloadSlider();
     })
 })
 
-let refreshSlider= setInterval(()=>next.click(),4000)
-function reloadSlider(){
-    let checkLeft=doorstepItem[active].offsetLeft
-    doorstepList.style.left=-checkLeft+'px'
-    let previousDot= document.querySelector('.doorstep-slider .dots li.active')
+let refreshSlider = setInterval(() => next.click(), 4000)
+function reloadSlider() {
+    let checkLeft = doorstepItem[active].offsetLeft
+    doorstepList.style.left = -checkLeft + 'px'
+    let previousDot = document.querySelector('.doorstep-slider .dots li.active')
     previousDot.classList.remove('active')
     dots[active].classList.add('active')
     clearInterval(refreshSlider);
-    refreshSlider= setInterval(()=>next.click(),4000)
+    refreshSlider = setInterval(() => next.click(), 4000)
 }
