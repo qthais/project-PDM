262369b719ecd4deb746c9c3c1d6b793553a0fcf
qthais <thaiallb63@gmail.com>
2024-05-05 22:06:01 +0700

    Handle for fewer product in cart

--- index.php
+++ index.php
@@ -25,19 +25,31 @@ if (isset($_POST["checkout"])) {
     echo $quantityOfSteeringWheel ."<br>";
     $quantityOfClutches = $_POST["Clutches"];
     echo $quantityOfClutches ."<br>";
-    $total= $quantityOfWheel+$quantityOfClutches+$quantityOfSteeringWheel;
 
+    // Calculate total quantity
+    $total = $quantityOfWheel + $quantityOfClutches + $quantityOfSteeringWheel;
 
-    $sqlIntoCart="INSERT INTO cart(TotalQuantity) 
-    VALUES($total)";
-    $resultIntoCart=mysqli_query($conn,$sqlIntoCart);
+    // Insert total quantity into cart table
+    $sqlIntoCart = "INSERT INTO cart(TotalQuantity) VALUES($total)";
+    $resultIntoCart = mysqli_query($conn, $sqlIntoCart);
     $cartID = mysqli_insert_id($conn);
-    //
-    $sqlIntoCartProduct="INSERT INTO cart_accessories (Cart_ID,Accessories_ID,Quantity)
-                            VALUES({$cartID},1,{$quantityOfWheel}),
-                                    ({$cartID},2,{$quantityOfSteeringWheel}),
-                                    ({$cartID},3,{$quantityOfClutches})";
 
-    $sqlIntoCartProduct=mysqli_query($conn,$sqlIntoCartProduct);
+    // Insert products into cart_accessories table
+    $sqlIntoCartProduct = "INSERT INTO cart_accessories (Cart_ID, Accessories_ID, Quantity) VALUES ";
+    if ($quantityOfWheel > 0) {
+        $sqlIntoCartProduct .= "($cartID, 1, $quantityOfWheel),";
+    }
+    if ($quantityOfSteeringWheel > 0) {
+        $sqlIntoCartProduct .= "($cartID, 2, $quantityOfSteeringWheel),";
+    }
+    if ($quantityOfClutches > 0) {
+        $sqlIntoCartProduct .= "($cartID, 3, $quantityOfClutches),";
+    }
+    // Remove the last comma from the SQL query string
+    $sqlIntoCartProduct = rtrim($sqlIntoCartProduct, ",");
+    
+    // Execute the query
+    $resultIntoCartProduct = mysqli_query($conn, $sqlIntoCartProduct);
 }
+
 ?>
